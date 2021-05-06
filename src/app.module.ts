b359b2456f7b0021f539b5f5fbfcb22c74b1db7b
mcnic <mcnic@ya.ru>
2021-05-07 00:27:24 +0300

    add postgres in docker-compose, config connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import AppController from "./app.controller";
 import AppService from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "test",
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export default class AppModule {}
