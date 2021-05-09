885b0e796b66363dceb2ba46cc0bc9be3ce0368c
mcnic <mcnic@ya.ru>
2021-05-09 11:58:59 +0300

    add swagger doc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateUserDto } from "src/users/dto/create-user.dto";
 import { AuthService } from "./auth.service";
 
@@ -8,11 +8,15 @@ import { AuthService } from "./auth.service";
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: "login" })
+  @ApiResponse({ status: 200 })
   @Post("login")
   login(@Body() userDto: CreateUserDto) {
     return this.authService.login(userDto);
   }
 
+  @ApiOperation({ summary: "registration" })
+  @ApiResponse({ status: 200 })
   @Post("registration")
   registration(@Body() userDto: CreateUserDto) {
     return this.authService.registration(userDto);
